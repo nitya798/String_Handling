b604cabe46ff3925c746611ba3e3138a58c15cb7
nitya798 <nitya.shukla_cs18@gla.ac.in>
2019-10-02 10:23:48 -0700

    add method main add scanner object add input variable add for loop print the output

--- src/main/Question8.java
+++ src/main/Question8.java
@@ -1,4 +1,19 @@
 package main;
 
+import java.util.Scanner;
 public class Question8 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();
+        int l = s1.length();
+        int sum = 0;
+        for (int i = 0; i < l; i++) {
+            char ch = s1.charAt(i);
+            if (ch >= 65 && ch <= 97) {
+                sum = sum + (int) ch;
+            }
+
+        }
+        System.out.println("upper weight: " + sum);
+    }
 }
--- src/main/Question9.java
+++ src/main/Question9.java
@@ -1,4 +1,23 @@
 package main;
 
+import java.util.Scanner;
 public class Question9 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();
+        int l = s1.length();
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int i = 0; i < l; i++) {
+            char ch = s1.charAt(i);
+            if (ch >= 65 && ch <= 90) {
+                sum1 = sum1 + (int) ch;
+            } else if (ch >= 97 && ch <= 122) {
+                sum2 = sum2 + (int) ch;
+            }
+
+        }
+        int sum3 = sum1 - sum2;
+        System.out.println("Absolute weight: " + sum3);
+    }
 }
